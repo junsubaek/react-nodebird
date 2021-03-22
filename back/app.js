cc8f153638d0060b029534fb3997d8b32a93f836
백진수 <baekjinsu@baegjinsuui-MacBookAir.local>
2021-03-23 01:54:24 +0900

    fix

--- back/app.js
+++ back/app.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "nodebird.com", "http://15.164.28.40"],
+    origin: ["http://localhost:3000", "nodebird.com", "http://15.164.28.40/"],
     credentials: true,
   })
 );
